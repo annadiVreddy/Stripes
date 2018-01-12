8ce6736f0cd231f3504ba644679062b5feb662d2
annadiVreddy <vinorannadi@gmail.com>
2018-01-12 15:02:32 -0600

    edit

--- script.js
+++ script.js
@@ -1827,46 +1827,6 @@ function submitData() {
 
     }
 
-    /* 
-      HTML to Microsoft Word Export Demo 
-  * This code demonstrates how to export an html element to Microsoft Word
-  * with CSS styles to set page orientation and paper size.
-  * Tested with Word 2010, 2013 and FireFox, Chrome, Opera, IE10-11
-  * Fails in legacy browsers (IE<10) that lack window.Blob object
-  
- window.export.onclick = function() {
- 
-   if (!window.Blob) {
-      alert('Your legacy browser does not support this action.');
-      return;
-   }
-
-   var html, link, blob, url, css;
-   
-   // EU A4 use: size: 841.95pt 595.35pt;
-   // US Letter use: size:11.0in 8.5in;
-   
-   css = (
-     '<style>' +
-     '@page WordSection1{size: 841.95pt 595.35pt;mso-page-orientation: landscape;}' +
-     'div.WordSection1 {page: WordSection1;}'+'.h3andamount{font-size: 2em;}'+'</style>'
-   );
-   
-   html = window.div2.innerHTML;
-   blob = new Blob(['\ufeff', css + html], {
-     type: 'application/msword'
-   });
-   url = URL.createObjectURL(blob);
-   link = document.createElement('A');
-   link.href = url;
-   // Set default file name. 
-   // Word will append file extension - do not add an extension here.
-   link.download = 'Vinod';   
-   document.body.appendChild(link);
-   if (navigator.msSaveOrOpenBlob ) navigator.msSaveOrOpenBlob( blob, 'Vinod.doc'); // IE10-11
-   		else link.click();  // other browsers
-   document.body.removeChild(link);
- };*/
 
 
 
@@ -1875,4 +1835,12 @@ function submitData() {
 function docuPrint() {
     document.title = document.getElementById('userEnteredWorkAddressId').value;
     window.print();
+}
+function backToEdit(){
+    if (document.getElementById('div2')) {
+
+        document.getElementById('div2').style.display = 'none';
+        document.getElementById('div1').style.display = 'block';
+
+    }
 }
